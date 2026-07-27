4a487020a8742e3471da5a90d7178b30c6a2c5bd
dohnnyj3pp <dohnnypuppet@gmail.com>
2026-07-27 00:01:06 -0600

    fixxing a break

--- js/cursor.js
+++ js/cursor.js
@@ -53,8 +53,7 @@ document.addEventListener("pointerover", (event) => {
 
     // Center ring on element
     ring.style.transform =
-  `translate(${rect.left + rect.width / 2 + window.scrollX}px, ${rect.top + rect.height / 2 + window.scrollY}px) translate(-50%, -50%) scale(${isSnapped ? 1.05 : 1})`;
-
+      `translate(${rect.left + rect.width / 2}px, ${rect.top + rect.height / 2}px) translate(-50%, -50%)`;
 
     ring.classList.add("snap");
   }
